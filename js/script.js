6350259ea329003b0bcf00b35ee5e53e714fc435
MaryMargaretWanjiku <marymegg.wanjiku@gmail.com>
2020-02-17 09:10:01 +0300

    link js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ function getName(){
     var year=parseInt(document.getElementById("year").value);
     var male=document.getElementById("male");
     var female=document.getElementById("female");   
-}
+
 
 if(day<=0||day>31){
-    alert("please enter a valid date!)
+    alert("please enter a valid date!")
 }
 else if(month<=0||month>12){
     alert("please enter a valid month!")
@@ -20,7 +20,11 @@ else if(year>2020){
     alert("please enter a valid year!")
 }
 var day= new Date(year + "/" + month + "/" + day);
-var birthday=day.getDay();
+var birthDay=day.getDay();
 if(male.checked==true){
-    document.getElementById("result").innerHTML="You were born on a "+daysOfWeek[birthDay]+ "your Akhan name is"+ malesNames[birthDay]
+    document.getElementById("result").innerHTML=("You were born on a "+ daysOfWeek[birthDay]+ "your Akhan name is"+ malesNames[birthDay])
+}
+else if(female.checked==true){
+    document.getElementById("result").innerHTML=("You were born on a "+ daysOfWeek[birthDay]+ "your Akhan name is"+ femalesNames[birthDay])
+}
 }
